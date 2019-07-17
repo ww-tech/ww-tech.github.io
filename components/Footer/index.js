df1972dda738158db926e14d7ef666d28c8985bd
Muhtasim Chowdhury <muhtasim.chowdhury@weigthwatchers.com>
2019-07-17 16:39:17 -0400

    added footer and neccessary css changes to accomodate it

---
+++ components/Footer/index.js
@@ -0,0 +1,7 @@
+import styles from './styles.scss'
+
+function Footer() {
+    return <div id='footer' className={styles.footer}>Footer</div>
+}
+
+export default Footer
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -19,6 +20,7 @@ class MyApp extends App {
       <Container>
           <Navbar/>
           <Component {...pageProps} />
+          <Footer/>
       </Container>
     );
   }
