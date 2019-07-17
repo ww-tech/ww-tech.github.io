53bc5c4cd30a625b1c5c2145f2400abf0430e9af
Muhtasim Chowdhury <muhtasim.chowdhury@weigthwatchers.com>
2019-07-17 13:35:56 -0400

    added github icon

--- pages/repos.js
+++ pages/repos.js
@@ -15,12 +15,13 @@ function Repos(data) {
           <div id='repo' key={repo.id} className={styles.repo}>
             <span id='name' className={styles.name}>{repo.name}</span>
             <div id='properties' className={styles.properties}>
-              <span>{repo.description}</span>
-              <span>{repo.language}</span>
-              <GitHubButton href={`https://github.com/ww-tech/${repo.name}/subscription`} data-icon="octicon-eye" data-show-count="true" aria-label={`Watch ww-tech/${repo.name} on GitHub`}>Watch</GitHubButton>
-              <GitHubButton href={`https://github.com/ww-tech/${repo.name}/fork`} data-icon="octicon-repo-forked" data-show-count="true" aria-label={`Fork ww-tech/${repo.name} on GitHub`}>Fork</GitHubButton>
-              <GitHubButton href={`https://github.com/ww-tech/${repo.name}`} data-icon="octicon-star" data-show-count="true" aria-label={`Star ww-tech/${repo.name} on GitHub`}>Star</GitHubButton>
-              <a href={repo.html_url}>{repo.html_url}</a>
+              <span className={styles.property}>{repo.description}</span>
+              <span className={styles.property}>{repo.language}</span>
+              <GitHubButton className={styles.property} href={`https://github.com/ww-tech/${repo.name}/subscription`} data-icon="octicon-eye" data-show-count="true" aria-label={`Watch ww-tech/${repo.name} on GitHub`}>Watch</GitHubButton>
+              <GitHubButton className={styles.property} href={`https://github.com/ww-tech/${repo.name}/fork`} data-icon="octicon-repo-forked" data-show-count="true" aria-label={`Fork ww-tech/${repo.name} on GitHub`}>Fork</GitHubButton>
+              <GitHubButton className={styles.property} href={`https://github.com/ww-tech/${repo.name}`} data-icon="octicon-star" data-show-count="true" aria-label={`Star ww-tech/${repo.name} on GitHub`}>Star</GitHubButton>
+              <a className={styles.property} href={repo.html_url}><img src='/static/github-icon.png' alt='github icon' width='20px' /></a>
+              
             </div>
           </div>
         )
