98b496c6208491a751e08e9acbec4cf11fc00b6b
matthew-rubensteinww <matthew.rubenstein@weightwatchers.com>
2019-08-02 10:32:59 -0400

    Deploy Next.js to gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0266d86687764dfd45d2";
+/******/ 	var hotCurrentHash = "46cf6f8a44146e2c216c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
